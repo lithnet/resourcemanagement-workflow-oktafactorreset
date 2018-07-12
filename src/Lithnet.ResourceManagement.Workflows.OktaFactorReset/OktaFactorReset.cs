07505aa2dd773208efbb913efba0e491a095e055
ryannewington <ryan@lithiumblue.com>
2018-07-12 10:41:43 +1000

    Initial commit

---
+++ src/Lithnet.ResourceManagement.Workflows.OktaFactorReset/OktaFactorReset.cs
@@ -0,0 +1,273 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Configuration;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Workflow.Activities;
+using System.Workflow.ComponentModel;
+using System.Xml.Serialization;
+using Microsoft.ResourceManagement.WebServices.WSResourceManagement;
+using Newtonsoft.Json.Linq;
+
+namespace Lithnet.ResourceManagement.Workflows
+{
+    public partial class OktaFactorReset : SequenceActivity
+    {
+        // This is the guid for the FIM Service built-in admin account.
+        // We will use it to execute each of our workflow activities.
+        private static Guid FimServiceGuid = new Guid("e05d1f1b-3d5e-4014-baa6-94dee7d68c89");
+
+        private List<RequestStatusDetail> StatusItems = new List<RequestStatusDetail>();
+
+        internal const string AllowedFactorTypesPropertyName = "AllowedFactorTypes";
+        internal const string OktaIdAttributeNamePropertyName = "OktaIdAttributeName";
+        internal const string TenantUrlPropertyName = "TenantUrl";
+        internal const string ApiKeyPropertyName = "ApiKey";
+
+
+        public static DependencyProperty TenantUrlProperty = DependencyProperty.Register(OktaFactorReset.TenantUrlPropertyName, typeof(string), typeof(OktaFactorReset));
+
+        [Description("The URL of the Okta tenant")]
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Visible)]
+        [Browsable(true)]
+        public string TenantUrl
+        {
+            get
+            {
+                return (string)this.GetValue(OktaFactorReset.TenantUrlProperty);
+            }
+
+            set
+            {
+                this.SetValue(OktaFactorReset.TenantUrlProperty, value);
+            }
+        }
+
+        private string ApiKey { get; set; }
+
+        public static DependencyProperty AllowedFactorTypesProperty = DependencyProperty.Register(OktaFactorReset.AllowedFactorTypesPropertyName, typeof(string), typeof(OktaFactorReset));
+
+        [Description("Enter the types of providers that this activity can reset. Leave blank to allow the activity to reset all factor types. Otherwise, use the format {provider}/{type} and separate each entry on a new line. For example, OKTA/push or YUBICO/token:hardware")]
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Visible)]
+        [Browsable(true)]
+        public string AllowedFactorTypes
+        {
+            get
+            {
+                return (string)this.GetValue(OktaFactorReset.AllowedFactorTypesProperty);
+            }
+
+            set
+            {
+                this.SetValue(OktaFactorReset.AllowedFactorTypesProperty, value);
+            }
+        }
+
+        public static DependencyProperty OktaIdAttributeNameProperty = DependencyProperty.Register(OktaFactorReset.OktaIdAttributeNamePropertyName, typeof(string), typeof(OktaFactorReset));
+
+        [Description("The name of the attribute that contains the Okta ID of the resource")]
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Visible)]
+        [Browsable(true)]
+        public string OktaIdAttributeName
+        {
+            get
+            {
+                return (string)this.GetValue(OktaFactorReset.OktaIdAttributeNameProperty);
+            }
+
+            set
+            {
+                this.SetValue(OktaFactorReset.OktaIdAttributeNameProperty, value);
+            }
+        }
+
+        public OktaFactorReset()
+        {
+            this.InitializeComponent();
+        }
+
+        private void InitializeReadResource_ExecuteCode(object sender, EventArgs e)
+        {
+            // Set the Actor ID for the Read Activity to the FIM Admin GUID
+            this.ReadResource.ActorId = FimServiceGuid;
+
+            // Set the Resource to retrieve as the currently requested object.
+            // Note, you could also set this to the target ID of the containing workflow
+            this.ReadResource.ResourceId = this.CurrentRequest.CurrentRequest.Target.GetGuid();
+
+            // Set the selection parameters as the date attribute
+            this.ReadResource.SelectionAttributes = new string[] { this.OktaIdAttributeName };
+        }
+
+        private void ExecuteCode(object sender, EventArgs e)
+        {
+            try
+            {
+                // Get the object that was read using the Read Activity Resource into a ResourceType object
+                // In a real-world workflow, you would check that anything was returned at all.
+
+                if (this.ReadResource == null)
+                {
+                    throw new InvalidOperationException("The ReadResource object was null");
+                }
+
+                if (this.ReadResource.Resource == null)
+                {
+                    throw new InvalidOperationException("The Resource provided to the workflow was null");
+                }
+
+                ResourceType resource = this.ReadResource.Resource;
+
+                // Get the Okta ID from the resource object
+
+                string oktaID = resource[this.OktaIdAttributeName] as string;
+
+                if (oktaID == null)
+                {
+                    throw new InvalidOperationException("The user had a null oktaID");
+                }
+
+                this.ApiKey = ConfigurationManager.AppSettings["okta-factor-reset-api-key"];
+
+                if (this.ApiKey == null)
+                {
+                    throw new InvalidOperationException("The Okta API key was not present in the app.config file");
+                }
+
+                this.ResetFactorsForUser(oktaID);
+                this.UpdateStatusItems();
+            }
+            catch (Exception ex)
+            {
+                this.AddMessage(DetailLevel.Error, string.Format("Unexpected error resetting factors\r\n{0}", ex));
+                this.UpdateStatusItems();
+                throw;
+            }
+
+            if (this.StatusItems.Any(t => t.DetailLevel == DetailLevel.Error))
+            {
+                throw new ApplicationException(string.Format("One or more errors occurred processing the workflow\r\n{0}", string.Join("\r\n", this.StatusItems.Select(t => t.Message).ToArray())));
+            }
+        }
+
+        private void ResetFactorsForUser(string oktaID)
+        {
+            Trace.WriteLine(string.Format("Resetting factor for user {0}", oktaID));
+
+            WebClient client = new WebClient();
+            client.Headers.Add("Content-Type", "application/json");
+            client.Headers.Add("Accept", "application/json");
+            client.Headers.Add("Authorization", string.Format("SSWS {0}", this.ApiKey));
+
+            JArray o = this.GetFactorDataForUser(oktaID, client);
+
+            string[] factorsTypesToDelete = string.IsNullOrEmpty(this.AllowedFactorTypes) ? new string[0] : this.AllowedFactorTypes.Split(new string[] { "\r\n", ";", "," }, StringSplitOptions.RemoveEmptyEntries);
+            int deletedCount = 0;
+
+            foreach (JToken i in o)
+            {
+                string factorDescription = string.Format("{0}/{1}", i["provider"], i["factorType"]).ToLowerInvariant();
+                Trace.WriteLine(string.Format("Factor:{0}", factorDescription));
+
+                foreach (string factorTypeToDelete in factorsTypesToDelete)
+                {
+                    if (factorTypeToDelete == "*" || factorTypeToDelete.ToLowerInvariant() == factorDescription)
+                    {
+                        Trace.WriteLine(string.Format("Matched a factor to delete: {0}", factorTypeToDelete));
+                        this.DeleteFactor(factorDescription, (string)i["id"], oktaID, client);
+                        deletedCount++;
+                    }
+                }
+            }
+
+            if (deletedCount == 0)
+            {
+                this.AddMessage(DetailLevel.Information, "The user had no factors to reset that matched the given criteria");
+            }
+        }
+
+        private JArray GetFactorDataForUser(string oktaID, WebClient client)
+        {
+            Uri uri = new Uri(new Uri(this.TenantUrl), string.Format("/api/v1/users/{0}/factors", oktaID));
+
+            Trace.WriteLine(string.Format("Getting factors from endpoint {0}", uri));
+
+            byte[] result = client.DownloadData(uri);
+            string data = Encoding.Default.GetString(result);
+            Trace.WriteLine(string.Format("Got factor data: {0}", data));
+
+            return JArray.Parse(data);
+        }
+
+        private void DeleteFactor(string factorDescription, string factorID, string userID, WebClient client)
+        {
+            try
+            {
+                Uri uri = new Uri(new Uri(this.TenantUrl), string.Format("/api/v1/users/{0}/factors/{1}", userID, factorID));
+
+                Trace.WriteLine(string.Format("Deleting factor {0} {1}", factorDescription, uri));
+                byte[] result = client.UploadValues(uri, "DELETE", new NameValueCollection());
+
+                if (result == null || result.Length == 0)
+                {
+                    Trace.WriteLine("Delete factor completed");
+                }
+                else
+                {
+                    string result2 = Encoding.Default.GetString(result);
+                    Trace.WriteLine(string.Format("Delete factor completed with message: {0}", result2));
+                }
+
+                this.AddMessage(DetailLevel.Information, string.Format("Reset factor {0}", factorDescription));
+            }
+            catch (Exception ex)
+            {
+                this.AddMessage(DetailLevel.Error, string.Format("Failed to reset factor {0}\r\n{1}", factorDescription, ex));
+            }
+        }
+
+        private void AddMessage(DetailLevel level, string message)
+        {
+            this.StatusItems.Add(new RequestStatusDetail(level, message));
+        }
+
+        private void UpdateStatusItems()
+        {
+            if (this.StatusItems.Count == 0)
+            {
+                return;
+            }
+
+            List<UpdateRequestParameter> updates = new List<UpdateRequestParameter>();
+
+            foreach (RequestStatusDetail item in this.StatusItems)
+            {
+                UpdateRequestParameter updateRequestParameter = new UpdateRequestParameter();
+                updateRequestParameter.PropertyName = "RequestStatusDetail";
+                updateRequestParameter.Value = SerializeObject(item);
+                updateRequestParameter.Mode = UpdateMode.Insert;
+                updates.Add(updateRequestParameter);
+            }
+
+            this.UpdateResource.UpdateParameters = updates.ToArray();
+            this.UpdateResource.ActorId = FimServiceGuid;
+            this.UpdateResource.ResourceId = this.CurrentRequest.CurrentRequest.ObjectID.GetGuid();
+        }
+
+        private static string SerializeObject(object toSerialize)
+        {
+            XmlSerializer xmlSerializer = new XmlSerializer(toSerialize.GetType());
+
+            using (StringWriter textWriter = new StringWriter())
+            {
+                xmlSerializer.Serialize(textWriter, toSerialize);
+                return textWriter.ToString();
+            }
+        }
+    }
+}
---
+++ src/Lithnet.ResourceManagement.Workflows.OktaFactorReset/OktaFactorResetUI.cs
@@ -0,0 +1,235 @@
+﻿using System.Web.UI.WebControls;
+using System.Workflow.ComponentModel;
+using Microsoft.IdentityManagement.WebUI.Controls;
+using Microsoft.ResourceManagement.Workflow.Activities;
+
+namespace Lithnet.ResourceManagement.Workflows
+{
+    public class OktaFactorResetUI : ActivitySettingsPart
+    {
+        private const string AllowedFactorTypesID = "controlAllowedFactorTypes";
+        private const string TenantUrlID = "controlTenantUrl";
+        private const string OktaIdAttributeNameID = "controlOktaIdAttributeName";
+
+        /// <inheritdoc />
+        /// <summary>
+        ///  Creates a Table that contains the controls used by the activity UI
+        ///  in the Workflow Designer of the FIM portal. Adds that Table to the
+        ///  collection of Controls that defines each activity that can be selected
+        ///  in the Workflow Designer of the FIM Portal. Calls the base class of 
+        ///  ActivitySettingsPart to render the controls in the UI.
+        /// </summary>
+        protected override void CreateChildControls()
+        {
+            Table controlLayoutTable = new Table
+            {
+                Width = Unit.Percentage(100.0),
+                BorderWidth = 0,
+                CellPadding = 2
+            };
+
+            controlLayoutTable.Rows.Add(this.AddTableRowTextBox("Tenant URL", OktaFactorResetUI.TenantUrlID, 400, 0, false, false, null));
+            controlLayoutTable.Rows.Add(this.AddTableRowDescription("Specify the full URL of the Okta tenant (Ensure the URL does not contain -admin after the tenant name)"));
+            controlLayoutTable.Rows.Add(new TableRow());
+
+            controlLayoutTable.Rows.Add(this.AddTableRowTextBox("Okta ID attribute name", OktaFactorResetUI.OktaIdAttributeNameID, 400, 0, false, false, null));
+            controlLayoutTable.Rows.Add(this.AddTableRowDescription("Specify the system name of the attribute that contains the user's Okta ID"));
+            controlLayoutTable.Rows.Add(new TableRow());
+
+            controlLayoutTable.Rows.Add(this.AddTableRowTextBox("Factors to reset", OktaFactorResetUI.AllowedFactorTypesID, 400, 0, true, false, null));
+            controlLayoutTable.Rows.Add(this.AddTableRowDescription("Enter the types of providers that this activity can reset. Use '*' to allow the activity to reset all factor types. Otherwise, use the format {provider}/{type} and separate each entry on a new line. For example, OKTA/push or YUBICO/token:hardware"));
+            controlLayoutTable.Rows.Add(new TableRow());
+
+            this.Controls.Add(controlLayoutTable);
+            base.CreateChildControls();
+        }
+
+        private TableRow AddTableRowTextBox(string labelText, string controlID, int width, int maxLength, bool multiLine, bool password, string defaultValue)
+        {
+            TableCell cell;
+            TableRow row = new TableRow();
+
+            Label label = new Label();
+            label.Text = labelText;
+            label.CssClass = this.LabelCssClass;
+
+            cell = new TableCell();
+            cell.Controls.Add(label);
+            row.Cells.Add(cell);
+
+            TextBox text = new TextBox();
+            text.ID = controlID;
+            text.CssClass = this.TextBoxCssClass;
+            text.Text = defaultValue;
+
+            if (maxLength > 0)
+            {
+                text.MaxLength = maxLength;
+            }
+
+            text.Width = width;
+
+            if (multiLine)
+            {
+                text.TextMode = TextBoxMode.MultiLine;
+                text.Rows = 6;
+                text.Wrap = true;
+            }
+
+            if (password)
+            {
+                text.TextMode = TextBoxMode.Password;
+            }
+
+            cell = new TableCell();
+            cell.Controls.Add(text);
+            row.Cells.Add(cell);
+            return row;
+        }
+
+        private TableRow AddTableRowDescription(string labelText)
+        {
+            TableCell cell;
+            TableRow row = new TableRow();
+            
+            Label label = new Label();
+            label.Text = labelText;
+            label.CssClass = this.LabelCssClass;
+
+            cell = new TableCell();
+            row.Cells.Add(cell);
+
+            cell = new TableCell();
+            cell.Controls.Add(label);
+            row.Cells.Add(cell);
+
+            return row;
+        }
+
+        private string GetText(string textBoxID)
+        {
+            TextBox textBox = (TextBox)this.FindControl(textBoxID);
+            return textBox.Text ?? string.Empty;
+        }
+
+        private void SetText(string textBoxID, string text)
+        {
+            TextBox textBox = (TextBox)this.FindControl(textBoxID);
+            if (textBox != null)
+            {
+                textBox.Text = text;
+            }
+        }
+
+        private void SetControlReadOnlyOption(string textBoxID, bool readOnly)
+        {
+            TextBox control = this.FindControl(textBoxID) as TextBox;
+
+            if (control != null)
+            {
+                control.ReadOnly = readOnly;
+            }
+        }
+
+        /// <summary>
+        /// Called when a user clicks the Save button in the Workflow Designer. 
+        /// Returns an instance of the RequestLoggingActivity class that 
+        /// has its properties set to the values entered into the text box controls
+        /// used in the UI of the activity. 
+        /// </summary>
+        public override Activity GenerateActivityOnWorkflow(SequentialWorkflow workflow)
+        {
+            if (!this.ValidateInputs())
+            {
+                return null;
+            }
+
+            return new OktaFactorReset
+            {
+                TenantUrl = this.GetText(OktaFactorResetUI.TenantUrlID),
+                AllowedFactorTypes = this.GetText(OktaFactorResetUI.AllowedFactorTypesID),
+                OktaIdAttributeName = this.GetText(OktaFactorResetUI.OktaIdAttributeNameID)
+            };
+        }
+
+        /// <summary>
+        /// Called by FIM when the UI for the activity must be reloaded.
+        /// It passes us an instance of our workflow activity so that we can
+        /// extract the values of the properties to display in the UI.
+        /// </summary>
+        public override void LoadActivitySettings(Activity activity)
+        {
+            var resetActivity = activity as OktaFactorReset;
+
+            if (resetActivity != null)
+            {
+                this.SetText(OktaFactorResetUI.TenantUrlID, resetActivity.TenantUrl);
+                this.SetText(OktaFactorResetUI.AllowedFactorTypesID, resetActivity.AllowedFactorTypes);
+                this.SetText(OktaFactorResetUI.OktaIdAttributeNameID, resetActivity.OktaIdAttributeName);
+            }
+        }
+
+        /// <summary>
+        /// Saves the activity settings.
+        /// </summary>
+        public override ActivitySettingsPartData PersistSettings()
+        {
+            ActivitySettingsPartData data = new ActivitySettingsPartData();
+            data[OktaFactorReset.AllowedFactorTypesPropertyName] = this.GetText(OktaFactorResetUI.AllowedFactorTypesID);
+            data[OktaFactorReset.TenantUrlPropertyName] = this.GetText(OktaFactorResetUI.TenantUrlID);
+            data[OktaFactorReset.OktaIdAttributeNamePropertyName] = this.GetText(OktaFactorResetUI.OktaIdAttributeNameID);
+            return data;
+        }
+
+        /// <summary>
+        ///  Restores the activity settings in the UI
+        /// </summary>
+        public override void RestoreSettings(ActivitySettingsPartData data)
+        {
+            if (data == null)
+            {
+                return;
+            }
+
+            this.SetText(OktaFactorResetUI.AllowedFactorTypesID, (string)data[OktaFactorReset.AllowedFactorTypesPropertyName]);
+            this.SetText(OktaFactorResetUI.TenantUrlID, (string)data[OktaFactorReset.TenantUrlPropertyName]);
+            this.SetText(OktaFactorResetUI.OktaIdAttributeNameID, (string)data[OktaFactorReset.OktaIdAttributeNamePropertyName]);
+        }
+
+        /// <summary>
+        ///  Switches the activity between read only and read/write mode
+        /// </summary>
+        public override void SwitchMode(ActivitySettingsPartMode mode)
+        {
+            bool readOnly = mode == ActivitySettingsPartMode.View;
+            this.SetControlReadOnlyOption(OktaFactorResetUI.AllowedFactorTypesID, readOnly);
+            this.SetControlReadOnlyOption(OktaFactorResetUI.TenantUrlID, readOnly);
+            this.SetControlReadOnlyOption(OktaFactorResetUI.OktaIdAttributeNameID, readOnly);
+        }
+
+        public override string Title
+        {
+            get
+            {
+                return "Reset Okta factors activity";
+            }
+        }
+
+        /// <summary>
+        ///  In general, this method should be used to validate information entered
+        ///  by the user when the activity is added to a workflow in the Workflow
+        ///  Designer.
+        ///  We could add code to verify that the log file path already exists on
+        ///  the server that is hosting the FIM Portal and check that the activity
+        ///  has permission to write to that location. However, the code
+        ///  would only check if the log file path exists when the
+        ///  activity is added to a workflow in the workflow designer. This class
+        ///  will not be used when the activity is actually run.
+        ///  For this activity we will just return true.
+        /// </summary>
+        public override bool ValidateInputs()
+        {
+            return true;
+        }
+    }
+}
